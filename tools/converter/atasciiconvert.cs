5273d32d9acd875fd1a89356a0293b2877fce65b
charlierobson <charlie.robson@b2m-solutions.com>
2017-09-18 10:55:22 +0100

    add atasci converter, c sharp source for MONO

---
+++ tools/converter/atasciiconvert.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace converter
+{
+    static class cvt
+    {
+        static void Main(string[] args)
+        {
+            var crlf = new byte[]{0x0d,0x0a};
+
+            foreach (var file in args)
+            {
+                var content = File.ReadAllBytes(file);
+                var converted = new List<byte>();
+                foreach(var b in content)
+                {
+                    if (b == 0x7f) converted.Add(9);
+                    else if (b == 0x9b) converted.AddRange(crlf);
+                    else converted.Add(b);
+                }
+                File.WriteAllBytes(Path.ChangeExtension(file, ".txt"), converted.ToArray());
+            }
+        }
+    }
+}
