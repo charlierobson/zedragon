301ebb0eef0f8a897b5c0be5c60657c97b2820fb
charlierobson <charlie.robson@b2m-solutions.com>
2017-10-10 16:26:21 +0100

    set iy=$3f00

--- datafns.asm
+++ datafns.asm
@@ -1,8 +1,42 @@
+; memory at $3f00
+;
+; $3f00 - $3f13  mul-by-600 table
+; $3f14 - $3f27
+; $3f28          reserved
+; $3f29 - $3f3a
+; $3f3b          reserved
+; $3f3c - $3f3f
+; $3f40 - $3fff  task data blocks
+
 inittables:
+    xor     a
+    ld      hl,$3f00
+    ld      bc,$100
+    call    zeromem
+
     ld      hl,mul600
-    ld      de,$403c
+    ld      de,$3f00
     ld      bc,20
     ldir
+
+    ld      a,(iy+$28)
+    ld      ($3f28),a
+    ld      a,(iy+$3b)
+    ld      ($3f3b),a
+
+    ld      iy,$3f00
+    ret
+
+
+mulby600:
+    sla     a
+    ld      (toff1),a
+    inc     a
+    ld      (toff2),a
+toff1 = $+2
+    ld      e,(iy+0)
+toff2 = $+2
+    ld      d,(iy+0)
     ret
 
 
@@ -15,16 +49,3 @@ updatecounter:
 +:  dec     a           ; the dec is always performed which is why reset value was bumped
     ld      (hl),a
     ret                 ; we return with Z set when the counter has reached zero
-
-
-mulby600:
-    sla     a
-    add     a,$3c
-    ld      (toff1),a
-    inc     a
-    ld      (toff2),a
-toff1 = $+2
-    ld      e,(iy+0)
-toff2 = $+2
-    ld      d,(iy+0)
-    ret
